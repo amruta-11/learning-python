5c32305b9d3c9a30a64d7da5468e9fbe93b3c769
Amruta Jadhav <jadhavamruta1196@gmail.com>
2019-11-06 21:53:45 -0800

    Name changed

--- IfEsleStatementBmi.py
+++ IfEsleStatementBmi.py
@@ -2,13 +2,13 @@ def CalculateBMI(weight_kg:float, height_meters: float):
     bmi = weight_kg / (height_meters ** 2)
     return bmi
 
-name = "Sam"
+name = "John Doe"
 height = 1.7
 weight = 71
 
-samBmi = CalculateBMI(weight, height)
-print("BMI of", name, "is", samBmi)
-if samBmi < 25:
+bmi = CalculateBMI(weight, height)
+print("BMI of", name, "is", bmi)
+if bmi < 25:
     print(name, "is not overweight")
 else:
     print(name, "is overweight")
